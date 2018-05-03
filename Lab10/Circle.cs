ebe699a0e69b0106e4711b79203a226734b074b2
jgeorge247 <justingeorge247@gmail.com>
2018-05-03 06:27:55 -0400

    first commit

---
+++ Lab10/Circle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Circle
+    {
+        private double radius;
+
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double CalculateCircumference()
+        {
+            double Circumference = (2 * (Math.PI) * Radius);
+            return Circumference;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            double Circumference = (2 * (Math.PI) * Radius);
+            return ($"{Circumference, 0:N2}");
+        }
+
+        public double CalculateArea ()
+        {
+            double Area = ((Math.PI) * Math.Pow(Radius, 2));
+            return Area;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            double Area = ((Math.PI) * Math.Pow(Radius, 2));
+            return ($"{Area, 0:N2}");
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            bool GoodCircle = true;
+            while (GoodCircle)
+            {
+                Console.WriteLine("Welcome to the Circle Tester");
+                Console.WriteLine("Please enter a radius: ");
+
+                string Input = Console.ReadLine();
+                double Radius;
+
+                while (!double.TryParse(Input, out Radius))
+                {
+                    Console.WriteLine("That was invalid. Please try again.");
+                    Input = Console.ReadLine();
+                }
+
+                Circle Justin = new Circle(Radius);
+                counter++;
+
+                Console.WriteLine($"The circle's area is {Justin.CalculateFormattedArea()}");
+                Console.WriteLine($"The circle's circumference is {Justin.CalculateFormattedCircumference()}");
+
+                Console.WriteLine("Would you like to continue? (Y/N)");
+                string Response = Console.ReadLine();
+
+                if (Response.ToUpper() != "Y" && Response.ToUpper() != "N")
+                {
+                    Console.WriteLine("That was not a valid response. Please try again");
+                    Response = Console.ReadLine();
+                }
+                else if (Response.ToUpper() == "N")
+                {
+                    Console.WriteLine($"Goodbye! You created {counter} circle objects");
+                    GoodCircle = false;
+                }
+                else if (Response.ToUpper() == "Y")
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
